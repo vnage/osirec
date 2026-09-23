2db9a8545bf5c7e87704d309fcfcd9846c61fde0
Yarik528 <fonarik@duck.com>
2026-09-23 11:46:00 +0700

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,22 +1,24 @@
+# banner.py
 
-CYAN = '\033[96m'
+RED = '\033[91m'
 BOLD = '\033[1m'
 RESET = '\033[0m'
 
-OSIREC_BANNER = f"""{CYAN}{BOLD}
-     ___       ____  _____ 
-    / _ \ _ __/ ___||  ___|
-   | | | | '_ \___ \| |_   
-   | |_| | |_) |__) |  _|  
-    \___/| .__/____/|_|    
-         |_|               
+OSIREC_BANNER = rf"""{RED}{BOLD}
+  ___  ____  _____ 
+ / _ \/ ___||  ___|
+| | | \___ \| |_   
+| |_| |___) |  _|  
+ \___/|____/|_|    
+                   
 {RESET}"""
 
-AUTHORS_LINE = f"{CYAN}Authors: feeraSe & Yarik528{RESET}"
+AUTHORS_LINE = f"{RED}Authors: feeraSe & Yarik528{RESET}"
 SEPARATOR = "-" * 40
 
 
 def print_banner():
+    """Выводит приветственный баннер при запуске."""
     print(OSIREC_BANNER)
     print(AUTHORS_LINE)
     print(SEPARATOR)
